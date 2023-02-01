6f349d6e4df06d1f47a45c5d0e0863bb5274710b
chafikdecide <106655338+chafikdecide@users.noreply.github.com>
2023-02-01 15:00:22 +0100

    Update pinger.sh

--- pinger.sh
+++ pinger.sh
@@ -1,11 +1,16 @@
 ip=10.0.2
-
+online_count=0
+offline_count=0
 
 for i in {1..255}
 do
-  ping -c 1 $ip.$i
-  if [ $? = 0 ]
-  then echo $ip.$i >> /home/kali/Desktop/online.txt
-  else echo $ip.$i >> /home/kali/Desktop/offline.txt
-  fi
+ping -c 1 $ip.$i
+if [ $? = 0 ]
+then
+echo $ip.$i >> /home/kali/Desktop/online.txt
+online_count=$((online_count+1))
+else
+echo $ip.$i >> /home/kali/Desktop/offline.txt
+offline_count=$((offline_count+1))
+fi
 done
